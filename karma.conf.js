1e44d2f34b485a145cac653f94af82eaeeac2d1a
Bruno Sousa <bsdesousa@gmail.com>
2017-06-02 16:13:09 -0300

    junit

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -32,8 +33,17 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+              ? ['progress', 'coverage-istanbul', 'junit']
+              : ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: './test', // results will be saved as $outputDir/$browserName.xml 
+      outputFile: 'Teste.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile 
+//      suite: '', // suite will become the package name attribute in xml testsuite element 
+      useBrowserName: true, // add browser name to report and classes names 
+//      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element 
+//      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element 
+//      properties: {} // key value pair of properties to add to the <properties> section of the report 
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
